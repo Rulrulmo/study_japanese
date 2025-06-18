b99283acd39d43487cac990d9f0d511328ff623a
khmin <khmin@rsupport.com>
2025-06-18 17:59:47 +0900

    feat: 폴더구조 및 단어리스트 준비 단계 진행

---
+++ lib/models/word.dart
@@ -0,0 +1,11 @@
+class Word {
+  final String japanese;
+  final String romaji;
+  final String korean;
+
+  const Word({
+    required this.japanese,
+    required this.romaji,
+    required this.korean,
+  });
+}
